7b296800b27e0bae63997f1dba67b1f536c502a8
yu_m <koituha23@gmail.com>
2022-12-02 05:21:38 +0900

    Update

--- TerminalApp/Controller/TerminalViewController.swift
+++ TerminalApp/Controller/TerminalViewController.swift
@@ -11,8 +11,6 @@ class TerminalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     // storyboardからViewControllerを生成
