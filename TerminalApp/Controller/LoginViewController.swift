ac60c29f79eba350f2ed014270e476d2644bc6a8
yu_m <koituha23@gmail.com>
2022-12-08 05:15:16 +0900

    ・ボタンのAutoLayoutを変更（左右マージンを設定） ・ボタン押下時に入力不備があった際にアラート表示を追加（UIAlertViewController）

--- TerminalApp/Controller/LoginViewController.swift
+++ TerminalApp/Controller/LoginViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var hostname: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var port: UITextField!
+    
     @IBOutlet weak var connectButton: UIButton! {
         didSet {
             connectButton.addTarget(self, action: #selector(tapButton(_:)), for: .touchUpInside)
@@ -20,8 +25,20 @@ class LoginViewController: UIViewController {
     }
 
     @objc func tapButton(_: UIResponder) {
-        let tv = TerminalViewController.instantiate()
-        self.navigationController?.pushViewController(tv, animated: true)
+        if hostname.text == "" || username.text == "" || password.text == "" || port.text == "" {
+            let alert = UIAlertController(title: "入力エラー", message: "全ての項目は必須入力です", preferredStyle: .alert)
+            
+            let ok = UIAlertAction(title: "OK", style: .default) { (action) in
+                self.dismiss(animated: true, completion: nil)
+            }
+            
+            alert.addAction(ok)
+            
+            present(alert, animated: true, completion: nil)
+        } else {
+            let tv = TerminalViewController.instantiate()
+            self.navigationController?.pushViewController(tv, animated: true)
+        }
     }
     
 }
